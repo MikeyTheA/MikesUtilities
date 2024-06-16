cbf89df6b1163a82008d77ef938ded3cead0a64c
MikeyTheA <170099338+MikeyTheA@users.noreply.github.com>
2024-06-16 15:32:07 +0300

    remove debug statements

--- Always catch.js
+++ Always catch.js
@@ -3,10 +3,8 @@ hook('AttemptCapturePhase', (phase) => {
         return;
     }
     const pokemon = phase.getPokemon();
-    print('test');
     const oldRandSeedInt = pokemon.randSeedInt;
     pokemon.randSeedInt = () => {
-        print('working');
         return 0;
     };
 
