988bf0c21e687a299e65c1ed2237342b7135bab9
MikeyTheA <170099338+MikeyTheA@users.noreply.github.com>
2024-06-26 14:32:41 +0300

    Egg editor

--- build/Window.js
+++ build/Window.js
@@ -1,18 +1,186 @@
-var ShinyVariant = ['Common (Yellow Stars)', 'Rare (Blue Stars)', 'Epic (Red Stars)'];
+var ShinyVariant = Object.keys(PokeRogue.enums.VariantTier).filter(function (v) { return isNaN(Number(v)); });
+var EggTier = Object.keys(PokeRogue.enums.EggTier).filter(function (v) { return isNaN(Number(v)); });
+var EggSourceType = Object.keys(PokeRogue.enums.EggSourceType).filter(function (v) { return isNaN(Number(v)); });
+var Species = Object.keys(PokeRogue.enums.Species).filter(function (v) { return isNaN(Number(v)); });
 var EGG_SEED = PokeRogue.data.EGG_SEED;
 var updateEggList = function (scene) {
+    var _a, _b;
     var handler = scene.ui.getHandler();
     if (handler instanceof PokeRogue.ui.EggListUiHandler) {
-        scene.gameData.eggs.forEach(function (egg) {
-            handler.setEggDetails(egg);
-        });
         var cursor = handler.getCursor();
+        handler.setEggDetails((_b = (_a = getBattleScene()) === null || _a === void 0 ? void 0 : _a.gameData) === null || _b === void 0 ? void 0 : _b.eggs[cursor]);
         handler.eggListIconContainer.removeAll(true);
         handler.show([]);
         handler.setCursor(cursor);
         handler.iconAnimHandler.addOrUpdate(handler.eggListIconContainer.getAt(cursor), 2);
     }
 };
+addWindow("Egg Selector", function () {
+    var battleScene = getBattleScene();
+    if (battleScene === undefined || battleScene.gameData === undefined) {
+        ImGui.Text("Loading");
+        return;
+    }
+    var selectedEgg = data.getData("selectedEgg", 0);
+    if (ImGui.Selectable("All eggs", selectedEgg === 0, 0, new ImGui.Vec2(100, 13))) {
+        data.setData("selectedEgg", 0);
+        getWindowOpenAccess("Egg Editor")(true);
+    }
+    var current = 1;
+    battleScene.gameData.eggs.forEach(function (egg, eggindex) {
+        if ((current + 1) * 100 < ImGui.GetWindowContentRegionMax().x) {
+            current += 1;
+            ImGui.SameLine();
+        }
+        else {
+            current = 1;
+        }
+        if (ImGui.Selectable("".concat(eggindex + 1, " ").concat(PokeRogue.enums.Species[egg._species]), selectedEgg === eggindex + 1, 0, new ImGui.Vec2(100, 13))) {
+            data.setData("selectedEgg", eggindex + 1);
+            getWindowOpenAccess("Egg Editor")(true);
+        }
+    });
+}, {
+    open: false,
+    persistentOpen: true,
+    hidden: true
+});
+addWindow("Egg Editor", function () {
+    var battleScene = getBattleScene();
+    if (battleScene === undefined || battleScene.gameData === undefined) {
+        ImGui.Text("Loading");
+        return;
+    }
+    var selectedEgg = data.getData("selectedEgg", 0);
+    if (selectedEgg === 0) {
+        ImGui.Text("Currently editing all eggs.");
+        ImGui.Combo("Tier", data.getAccess("GlobalEggsTier", 0), EggTier, ImGui.ARRAYSIZE(EggTier));
+        ImGui.SameLine();
+        if (ImGui.Button("Set##TierGlobal")) {
+            battleScene.gameData.eggs.forEach(function (egg) {
+                egg._tier = data.getData("GlobalEggsTier", 0);
+                updateEggList(battleScene);
+                egg._id = PokeRogue.randInt(EGG_SEED, EGG_SEED * egg._tier);
+                egg._species = egg.rollSpecies(battleScene);
+            });
+        }
+        ImGui.Combo("Source Type", data.getAccess("GlobalEggsSourceType", 0), EggSourceType, ImGui.ARRAYSIZE(EggSourceType));
+        ImGui.SameLine();
+        if (ImGui.Button("Set##SourceGlobal")) {
+            battleScene.gameData.eggs.forEach(function (egg) {
+                egg._sourceType = data.getData("GlobalEggsSourceType", 0);
+                updateEggList(battleScene);
+                egg._species = egg.rollSpecies(battleScene);
+                egg._eggMoveIndex = egg.rollEggMoveIndex();
+            });
+        }
+        ImGui.Checkbox("Shiny", data.getAccess("GlobalEggsShiny", false));
+        ImGui.SameLine();
+        if (ImGui.Button("Set##Shiny")) {
+            battleScene.gameData.eggs.forEach(function (egg) {
+                egg._isShiny = data.getData("GlobalEggsShiny", false);
+                egg._variantTier = egg.rollVariant();
+            });
+        }
+        ImGui.Combo("Shiny variant", data.getAccess("GlobalEggsShinyVariant", false), ShinyVariant, ImGui.ARRAYSIZE(ShinyVariant));
+        ImGui.SameLine();
+        if (ImGui.Button("Set##ShinyVariant")) {
+            battleScene.gameData.eggs.forEach(function (egg) {
+                egg._variantTier = data.getData("GlobalEggsShinyVariant", false);
+            });
+        }
+        if (ImGui.Button("Set hatching to 0 waves")) {
+            battleScene.gameData.eggs.forEach(function (egg) {
+                egg._hatchWaves = 0;
+            });
+        }
+        return;
+    }
+    var egg = battleScene.gameData.eggs[selectedEgg - 1];
+    if (egg === undefined) {
+        ImGui.Text("Egg not found, select another one.");
+        return;
+    }
+    ImGui.Text("Currently editing egg ".concat(selectedEgg));
+    ImGui.Text("Species: ".concat(PokeRogue.enums.Species[egg._species]));
+    ImGui.SameLine();
+    if (ImGui.Button("Reroll")) {
+        egg._species = egg.rollSpecies(battleScene);
+    }
+    ImGui.SameLine();
+    ImGui.PushItemWidth(-15);
+    ImGui.InputText("##EditSpecies".concat(egg._id), function (value) {
+        if (value === void 0) { value = data.getData("editSpecies".concat(egg._id), ""); }
+        value = value.toUpperCase();
+        data.setData("editSpecies".concat(egg._id), value);
+        if (Species.includes(value)) {
+            egg._species = PokeRogue.enums.Species[value];
+            egg._tier = egg.getEggTierFromSpeciesStarterValue();
+        }
+        return value;
+    });
+    ImGui.PopItemWidth();
+    ImGui.SameLine();
+    if (Species.includes(data.getData("editSpecies".concat(egg._id), ""))) {
+        ImGui.PushStyleColor(ImGui.Col.Text, ImGui.IM_COL32(20, 255, 20, 255));
+        ImGui.Text("v");
+        ImGui.PopStyleColor();
+    }
+    else {
+        ImGui.PushStyleColor(ImGui.Col.Text, ImGui.IM_COL32(255, 20, 20, 255));
+        ImGui.Text("x");
+        ImGui.PopStyleColor();
+    }
+    ImGui.Combo("Tier", function (value) {
+        if (value === void 0) { value = egg._tier; }
+        var modified = value !== egg._tier;
+        egg._tier = value;
+        if (modified) {
+            updateEggList(battleScene);
+            egg._id = PokeRogue.randInt(EGG_SEED, EGG_SEED * egg._tier);
+            egg._species = egg.rollSpecies(battleScene);
+        }
+        return egg._tier;
+    }, EggTier, ImGui.ARRAYSIZE(EggTier));
+    ImGui.Combo("Source Type", function (value) {
+        if (value === void 0) { value = egg._sourceType; }
+        var modified = value !== egg._sourceType;
+        egg._sourceType = value;
+        if (modified) {
+            updateEggList(battleScene);
+            egg._tier = egg.rollEggTier();
+            egg._species = egg.rollSpecies(battleScene);
+            egg._eggMoveIndex = egg.rollEggMoveIndex();
+        }
+        return egg._sourceType;
+    }, EggSourceType, ImGui.ARRAYSIZE(EggSourceType));
+    ImGui.Checkbox("Shiny", function (value) {
+        if (value === void 0) { value = egg._isShiny; }
+        var modified = value !== egg._isShiny;
+        egg._isShiny = value;
+        if (modified)
+            egg._variantTier = egg.rollVariant();
+        return value;
+    });
+    ImGui.Combo("Shiny variant", function (value) {
+        if (value === void 0) { value = egg._variantTier; }
+        egg._variantTier = value;
+        return egg._variantTier;
+    }, ShinyVariant, ImGui.ARRAYSIZE(ShinyVariant));
+    ImGui.InputText("Waves remaining", function (value) {
+        if (value === void 0) { value = egg._hatchWaves; }
+        var modified = egg._hatchWaves !== value;
+        egg._hatchWaves = parseInt(value) || 0;
+        return String(egg._hatchWaves || 0);
+    }, 256, ImGui.InputTextFlags.CharsDecimal);
+    if (ImGui.Button("Set to 0 waves")) {
+        egg._hatchWaves = 0;
+    }
+}, {
+    open: false,
+    persistentOpen: false,
+    hidden: true
+});
 addWindow("Mike's utilities", function () {
     var battleScene = getBattleScene();
     ImGui.Checkbox('Free rerolls', data.getAccess('FreeRerolls', false, true));
@@ -54,6 +222,7 @@ addWindow("Mike's utilities", function () {
             return String(value || 0);
         }, 256, ImGui.InputTextFlags.CharsDecimal);
     }
+    ImGui.Checkbox("Egg editor", getWindowOpenAccess("Egg Selector"));
     if (battleScene && battleScene.gameData && battleScene.gameData.voucherCounts && ImGui.CollapsingHeader('Edit vouchers')) {
         var VoucherType = Object.keys(PokeRogue.system.VoucherType).filter(function (v) { return isNaN(Number(v)); });
         VoucherType.forEach(function (Voucher, VoucherId) {
@@ -70,51 +239,10 @@ addWindow("Mike's utilities", function () {
             }, 256, ImGui.InputTextFlags.CharsDecimal);
         });
     }
-    if (battleScene && battleScene.gameData && battleScene.gameData.eggs && ImGui.CollapsingHeader('Edit eggs')) {
-        /*ImGui.Checkbox('Always shiny (eggs)', data.getAccess('AlwaysShinyEggs', false, true));
-        if (data.getData('AlwaysShinyEggs', false, true)) {
-            ImGui.Text('  ');
-            ImGui.SameLine();
-            const currentShiny = data.getData('ShinyVariant', 0, true);
-            if (ImGui.BeginCombo('Shiny variant##eggs', ShinyVariant[currentShiny])) {
-                ShinyVariant.forEach((shinyVariant, n) => {
-                    const is_selected = currentShiny === n;
-                    if (ImGui.Selectable(shinyVariant, is_selected)) data.setData('ShinyVariant', n, true);
-                    if (is_selected) ImGui.SetItemDefaultFocus();
-                });
-                ImGui.EndCombo();
-            }
-        }*/
-        var EggTier = Object.keys(PokeRogue.enums.EggTier).filter(function (v) { return isNaN(Number(v)); });
-        EggTier.forEach(function (Tier, TierId) {
-            if (ImGui.Button("Set all eggs Tier to ".concat(Tier))) {
-                battleScene.gameData.eggs.forEach(function (egg) {
-                    var random_remainder = Math.floor(Math.random() * EGG_SEED);
-                    var new_id = TierId * EGG_SEED + random_remainder;
-                    egg._id = new_id;
-                    egg._tier = TierId;
-                });
-                updateEggList(battleScene);
-            }
-        });
-        var EggSourceType = Object.keys(PokeRogue.enums.EggSourceType).filter(function (v) { return isNaN(Number(v)); });
-        EggSourceType.forEach(function (Gacha, GachaId) {
-            if (ImGui.Button("Set all eggs GachaType to ".concat(Gacha))) {
-                battleScene.gameData.eggs.forEach(function (egg) {
-                    egg._sourceType = GachaId;
-                });
-                updateEggList(battleScene);
-            }
-        });
-        if (ImGui.Button('Set all eggs to hatch next wave')) {
-            battleScene.gameData.eggs.forEach(function (egg) {
-                egg.hatchWaves = 0;
-            });
-        }
-    }
     if (battleScene && battleScene.gameData && battleScene.gameData.saveSystem && ImGui.Button('Save Data')) {
         battleScene.gameData.saveSystem();
     }
 }, {
     persistentOpen: true,
 });
+ImGui;
